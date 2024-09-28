40cdeaea962196660cb5e487b5c683e0f651cad7
Olena Jerdeva <lena.jerdeva28@gmail.com>
2024-09-28 23:55:06 +0300

    cast and review added

--- films.js
+++ films.js
@@ -1,32 +1,103 @@
 const fs = require("fs").promises;
 const path = require("path");
 
-
-const filmsPath = path.join(__dirname, './films/films.json');
+const filmsPath = path.join(__dirname, "./films/films.json");
+const creditPath = path.join(__dirname, "./cast/cast.json");
+const reviewsPath = path.join(__dirname, "./reviews/reviews.json");
 
 async function filmsList() {
-    try {
-        const data = await fs.readFile(filmsPath, "utf-8");
-        const films = JSON.parse(data);
-        return films;
-    } catch (error) {
-        throw error;
-    }
+  try {
+    const data = await fs.readFile(filmsPath, "utf-8");
+    const films = JSON.parse(data);
+    return films;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function creditList() {
+  try {
+    const data = await fs.readFile(creditPath, "utf-8");
+    const credits = JSON.parse(data);
+    return credits;
+  } catch (error) {
+    throw error;
+  }
 }
 
 async function getFilmsId(filmsId) {
-    try {
-        const id = String(filmsId);
-        const films = await filmsList();
-        const film = films.find((film) => film.id === id);
-        return film || null;
-    } catch (error) {
-        throw error;
-    }
+  try {
+    const id = String(filmsId);
+    const films = await filmsList();
+    const film = films.find((film) => film.id === id);
+    return film || null;
+  } catch (error) {
+    throw error;
+  }
 }
+
+async function getFilmsByName(title) {
+  try {
+    const name = String(title);
+    const films = await filmsList();
+    const film = films.find((film) => film.title === name);
+    return film || null;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getMovieCredits(movieId) {
+  try {
+    const id = Number(movieId);
+
+    const credits = await creditList();
+
+    const credit = credits.find((credit) => credit.id === id);
+
+    if (!credit) {
+      console.log("Кредит не найден для данного ID.");
+      return null;
+    }
+
+    const castMembers = credit.cast;
+
+    return castMembers;
+  } catch (error) {
+    console.error("Ошибка:", error);
+    throw error;
+  }
+}
+
+async function getMovieReviews(movieId) {
+  try {
+    const id = Number(movieId);
+
+    const data = await fs.readFile(reviewsPath, "utf8");
+    const reviews = JSON.parse(data);
+
+    const review = reviews.find((review) => review.id === id);
+
+    if (!review) {
+      console.log("Рецензия не найдена для данного ID.");
+      return null;
+    }
+
+    const reviewItem = review.results;
+    return reviewItem;
+  } catch (error) {
+    console.error("Ошибка:", error);
+    throw error;
+  }
+}
+
+
+
 
 module.exports = {
-    filmsList,
-    getFilmsId
-}
-
+  filmsList,
+  getFilmsId,
+  getFilmsByName,
+  getMovieCredits,
+  getMovieReviews,
+};
--- index.js
+++ index.js
@@ -2,16 +2,31 @@ const yargs = require("yargs");
 const films = require("./films");
 const argv = require("yargs").argv;
 
-async function invokeAction({action, id}) {
+async function invokeAction({action, id, title}) {
     switch (action) {
       case "list":
         const filmsList = await films.filmsList();
-         console.log(filmsList);
+        console.log(filmsList);
         break;
 
       case "get":
         const getFilm = await films.getFilmsId(id);
-         console.log(getFilm);
+        console.log(getFilm);
+        break;
+
+      case "getByName":
+        const getFilmByName = await films.getFilmsByName(title);
+        console.log(getFilmByName);
+        break;
+
+      case "getMovieCredits":
+        const getMovieCredits = await films.getMovieCredits(id);
+        console.log(getMovieCredits);
+        break;
+
+      case "getMovieReviews":
+        const getMovieReviews = await films.getMovieReviews(id);
+        console.log(getMovieReviews);
         break;
 
       default:
